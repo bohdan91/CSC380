5e9b155e82f50998ec244b8eb7721cda60e84f28
bohdan91 <bohdan91@gmail.com>
2017-02-18 06:01:04 -0500

    Login window, create window, encryption, some junit test

---
+++ Password Manager/src/Main/FileManager.java
@@ -0,0 +1,114 @@
+package Main;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.security.Key;
+
+/**
+ * Class for managing db files, reading, writing, encrypting, decrypting
+ *
+ */
+public class FileManager {
+
+    private static String ALGO = "AES";
+    private static int keyLength = 16;
+    private File dbFile;
+    private String controlKey = "abc123";
+    private byte[] keyValue;
+
+    public FileManager(){
+
+    }
+
+    public boolean tryOpen(File file, byte[] pas){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            keyValue = new byte[keyLength];
+            int c = 0;
+            for(int i = 0; i < keyLength; i ++)
+            {
+                if(pas.length > c){
+                    keyValue[i] = pas[c];
+                    c++;
+                } else {
+                    c = 0;
+                }
+            }
+            dbFile = file;
+
+            String control = br.readLine();
+            String result = decrypt(control);
+            System.out.println(result);
+
+            if(result.equals(controlKey)){
+                br.close();
+                return true;
+            }
+
+            br.close();
+        } catch(Exception e){
+            System.err.println(e);
+        }
+        return false;
+    }
+
+    public boolean createNewDB(String name, byte[] pas, String path){
+        keyValue = new byte[keyLength];
+        int c = 0;
+        for(int i = 0; i < keyLength; i ++)
+        {
+            if(pas.length > c){
+                keyValue[i] = pas[c];
+                c++;
+            } else {
+                c = 0;
+            }
+        }
+        try{
+            dbFile = new File(path + "/" + name + ".db");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(dbFile));
+            bw.write(encrypt(controlKey));
+            bw.close();
+        }catch (Exception e ) {
+            System.err.println(e);
+        }
+
+        return true;
+    }
+
+    public String encrypt(String Data) throws Exception {
+        Key key = generateKey(keyValue);
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encVal = c.doFinal(Data.getBytes());
+        String encryptedValue = new BASE64Encoder().encode(encVal);
+        return encryptedValue;
+    }
+
+    public String decrypt(String encryptedData) throws Exception {
+        Key key = generateKey(keyValue);
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.DECRYPT_MODE, key);
+        byte[] decordedValue = new BASE64Decoder().decodeBuffer(encryptedData);
+        byte[] decValue = c.doFinal(decordedValue);
+        String decryptedValue = new String(decValue);
+        return decryptedValue;
+    }
+
+    //We use "generateKey()" method to generate a secret key for AES algorithm with a given key.
+    private Key generateKey(byte[] keyValue) throws Exception {
+        Key key = new SecretKeySpec(keyValue, ALGO);
+        return key;
+    }
+
+    public void setKey(byte[] key){
+        keyValue = key;
+    }
+
+    public int getKeyLength(){
+        return keyLength;
+    }
+}
---
+++ Password Manager/src/Tests/FileMenegerTest.java
@@ -0,0 +1,130 @@
+package Tests;
+import Main.FileManager;
+import org.junit.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.File;
+import java.util.Random;
+
+
+/**
+ * Created by Bohdan Yevdokymov
+ */
+public class FileMenegerTest {
+
+    private static FileManager file;
+    private static int keyLegth;
+    private static byte[] key;
+    private static File dbFile;
+
+
+    @BeforeClass
+    public static void initFileManager(){
+        file = new FileManager();
+        keyLegth = file.getKeyLength();
+        dbFile = new File(System.getProperty("user.dir") + "/" + "testFile2" + ".db");
+
+        key = new byte[keyLegth];
+        Random rand = new Random();
+
+        for(int i =0; i < keyLegth; i ++){
+
+            key[i] = (byte) rand.nextInt(10);
+        }
+
+        file.setKey(key);
+
+        try{
+            file.createNewDB("testFile2", key, System.getProperty("user.dir"));
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterClass
+    public static void finishTest(){
+        dbFile.delete();
+    }
+
+    @Test
+    public void testEncryptDecrypt (){
+        try {
+            String send     = "testmessage123";
+            String encoded  = file.encrypt(send);
+
+            Assert.assertNotEquals(send, encoded);
+
+            String recieved = file.decrypt(encoded);
+
+            Assert.assertEquals(send, recieved);
+
+        } catch (Exception e){
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testCreateNewDB(){
+
+        String fileName = "testFile";
+        String path     =  System.getProperty("user.dir");
+
+        file.createNewDB(fileName, key, path);
+
+        try{
+            File f = new File(path + "/" + fileName + ".db");
+            FileReader fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            br.readLine();
+            br.close();
+            fr.close();
+            f.delete();
+
+        }catch(Exception e){
+            Assert.fail();
+        }
+
+    }
+
+    @Test
+    public void testTryOpenPositive(){
+
+        Assert.assertTrue(file.tryOpen(dbFile, key));
+
+    }
+
+    @Test
+    public void testTryOpenNegative(){
+        byte []newKey = new byte[keyLegth];
+        Random rand = new Random();
+
+        for(int i =0; i < keyLegth; i ++){
+
+            newKey[i] = (byte) rand.nextInt(10);
+        }
+
+        Assert.assertFalse(file.tryOpen(dbFile, newKey));
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
