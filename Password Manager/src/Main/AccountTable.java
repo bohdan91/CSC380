059cc73d21a2eb9e1ca1705098b06fd7e6ec063c
Darkking271 <darking271@gmail.com>
2017-02-28 11:51:41 -0500

    AccountTable test updated

--- Password Manager/src/Main/AccountTable.java
+++ Password Manager/src/Main/AccountTable.java
@@ -6,56 +6,50 @@ import java.util.TreeSet;
 /**
  * Created by Alex Voytovich on 2/21/2017
  */
-public class AccountTable
-{
+public class AccountTable{
+	
     HashMap<String, Account> accountTable;
 
-    public AccountTable()
-    {
+    public AccountTable(){
         accountTable = new HashMap<>();
     }
 
-    public void put(String name, Account user)
-    {
+    public void put(String name, Account user){
         accountTable.put(name, user);
     }
     
-    public Account get(String name)
-    {
+    public Account get(String name){
     	return accountTable.get(name);
 
     }
 
-    public boolean contains(String name)
-    {
+    public boolean contains(String name){
         return accountTable.containsKey(name);
     }
 
-    public void remove(String name)
-    {
+    public int size(){
+    	return accountTable.size();
+    }
+    
+    public void remove(String name){
         accountTable.remove(name);
     }
 
-    public void clear()
-    {
+    public void clear(){
         accountTable.clear();
     }
 
-    public Set alphaSet()
-    {
+    public Set<String> alphaSet(){
         TreeSet<String> set = new TreeSet<>();
-        for (String s : accountTable.keySet())
-        {
+        for (String s : accountTable.keySet()){
             set.add(s);
         }
         return set;
     }
 
-    public Set typeSet(String type)
-    {
+    public Set<String> typeSet(String type){
         TreeSet<String> set = new TreeSet<>();
-        for (String s : accountTable.keySet())
-        {
+        for (String s : accountTable.keySet()){
             if (accountTable.get(s).getType().equals(type));
                 set.add(s);
         }
--- Password Manager/src/Tests/AccountTablePutTest.java
+++ Password Manager/src/Tests/AccountTablePutTest.java
@@ -3,6 +3,7 @@ package Tests;
 import Main.Account;
 import Main.AccountTable;
 import Main.FileManager;
+import Main.Main;
 
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -13,29 +14,26 @@ import javax.crypto.NoSuchPaddingException;
 
 import org.junit.*;
 
-public class AccountTablePutTest
-{
+public class AccountTablePutTest{
 	AccountTable table;
 	Account ac;
+	Account bc;
+	Account cc;
 	FileManager manage;
 	
     @Test
     public void testCreation()
     throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, 
-    NoSuchAlgorithmException, NoSuchPaddingException  
-    {
-        table = new AccountTable();
-        ac = new Account("Email", "Darkking271", "123456", 
-			     "Me Email", "Email", "Gmail.com");
-        manage = new FileManager();
+    NoSuchAlgorithmException, NoSuchPaddingException{
+        Main.fileManager = new FileManager();
+    	table = new AccountTable();
+        ac = new Account("Email", "darking271", "123456", "My Email", "Email", "Gmail.com");
+        bc = new Account("Bank", "avoytovich", "123123","My Bank Account", "Banking", "Chase.com");
+        cc = new Account("Steam", "Darkking271", "qwerty","My Gaming Account", "Gaming", "SteamPowered.com");
+        table.put(ac.getTitle(), ac);
+        Assert.assertEquals(3, table.size());
     }
     
-    @Test
-    public void testPut()
-    throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, 
-    NoSuchAlgorithmException, NoSuchPaddingException  
-    {
-    	table.put(ac.getTitle(), ac);
-    	Assert.assertEquals("Darkking271", table.get(ac.getTitle()).getTitle());
-    }
+    //@Test
+    //public void test
 }
