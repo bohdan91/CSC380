4178a5d8c39dc0ceddbd11156b5122368b887e8e
Darkking271 <darking271@gmail.com>
2017-03-16 17:08:23 -0400

    SQL class saved

--- Password Manager/src/Main/SQl.java
+++ Password Manager/src/Main/SQl.java
@@ -1,5 +1,26 @@
 package Main;
 
-public class SQl {
-
+public static class SQl {
+	
+	private String uniquID;
+	
+	public Sring run(String uniqueID){
+		this.uniquID  uniqueID;
+	}
+	
+	private void update(){
+		
+	}
+	
+	private String pull(){
+		
+	}
+	
+	private void push(){
+		
+	}
+	
+	private String getID(String userName){
+		
+	}
 }
