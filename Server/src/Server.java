c651fe14452e40a0331f08a8acb5ce51775f1328
Darkking271 <darking271@gmail.com>
2017-03-28 12:06:02 -0400

    Server Methods Structure
    Took 5 minutes

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,9 +1,23 @@
-
+import java.io.*;
+import java.net.*;
 
 class Server{
 
     public static void main(String[] args) {
 
     }
+
+    //run
+    public static void run(){
+
+    }
+    
+    //get encrypted
+
+    //compare
+
+    //update acnt
+
+    //get accounts
 
 }
