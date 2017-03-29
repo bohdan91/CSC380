d0f29c9b0ab2b1d40ffd429c1c35dd59479fbec4
bohdan91 <bohdan91@gmail.com>
2017-03-29 15:51:23 -0400

    Server/Client example

---
+++ Server/src/Client.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by Bohdan on 3/29/17.
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        try {
+            // Make connection and initialize streams
+            Socket socket = new Socket("127.0.0.1", 9898);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            // Consume the initial welcoming messages from the server
+            for (int i = 0; i < 3; i++) {
+                System.out.println(in.readLine() + "\n");
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                System.out.print("Enter message: ");
+                String s = br.readLine();
+                out.println(s);
+
+                String response = in.readLine();
+                System.out.println(response);
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
