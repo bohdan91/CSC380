1782a1d20a810b4c2f5e4dfc27ad576c02f3be78
bohdan91 <bohdan91@gmail.com>
2017-04-11 12:04:56 -0400

    Connection methods

--- Password Manager/src/Main/Connection.java
+++ Password Manager/src/Main/Connection.java
@@ -33,7 +33,7 @@ public class Connection {
         getInstance().closeConnection();
     }
 
-    public boolean openConnection(){
+    private boolean openConnection(){
         try{
             socket = new Socket(address, port);
             out = new ObjectOutputStream(socket.getOutputStream());
@@ -48,7 +48,7 @@ public class Connection {
         }
     }
 
-    public void closeConnection(){
+    private void closeConnection(){
         try {
             isConnected = false;
             out.close();
@@ -63,23 +63,83 @@ public class Connection {
         String[] request = new String[2];
         request[0] = "getEncryptedId";
         request[1] = username;
-        if(send(request)){
-            return receive()[0];
-        }else {
-            return null;
+        if(openConnection()) {
+            if (send(request)) {
+                String r = receive()[0];
+                closeConnection();
+                return r;
+            }
         }
+        closeConnection();
+        return null;
     }
 
     public boolean checkDecryptedId(String decrypted){
-        String[] request = new String[1];
-        request[0] = decrypted;
+        String[] request = new String[2];
+        request[0] = "checkDecryptedId";
+        request[1] = decrypted;
 
-        if(send(request)){
-            String response = receive()[0];
-            if(response.toLowerCase().equals("true"));{
-                return true;
+        if(openConnection()) {
+            if (send(request)) {
+                String response = receive()[0];
+                if (response.toLowerCase().equals("true")) ;
+                {
+                    return true;
+                }
             }
         }
+        return false;
+    }
+
+    public String[] getAccounts(String decryptedId){
+        String[] accounts = new String[0];
+        String[] request  = new String[2];
+        request[0] = "getAccounts";
+        request[1] = decryptedId;
+
+        if(openConnection()){
+            if(send(request)){
+                accounts = receive();
+            }
+        }
+        closeConnection();
+        return accounts;
+    }
+
+    public boolean isUserAvailable(String username){
+        String[] request = new String[2];
+        request[0] = "isUserAvailable";
+        request[1] = username;
+
+        if(openConnection()){
+            if(send(request)){
+                String response = receive()[0];
+                if(response.toLowerCase().equals("true")){
+                    closeConnection();
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean registerUser(String username, String encryptedId, String decryptedId){
+        String[] request = new String [4];
+        request[0] = "registerUser";
+        request[1] = username;
+        request[2] = encryptedId;
+        request[3] = decryptedId;
+
+        if(openConnection()){
+            if(send(request)){
+                String response = receive()[0];
+                if(response.toLowerCase().equals("true"));{
+                    closeConnection();
+                    return true;
+                }
+            }
+        }
+        closeConnection();
         return false;
     }
 
