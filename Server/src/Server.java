e3e6a5fbae37a959f47dc15ab433af98200874a5
Darkking271 <darking271@gmail.com>
2017-04-24 12:36:03 -0400

    Update
    Socket updated for connection class.
    Documentation added to server app.

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -3,8 +3,26 @@ import java.io.*;
 import java.net.*;
 import java.sql.*;
 
+/**
+ * Created by Alex and Bohdan
+ *
+ * Server
+ *
+ * A simple server program. On start it runs infinitely, and listens for a user
+ * connection. When the user connects, it reroutes them to the service class.
+ */
 public class Server {
 
+    /**
+     * Main Method
+     *
+     * The main class for the server program. This method connects to the sqlite
+     * database, then proceeds to loop infinitely until the server program is terminated.
+     * During the infinite loop, it listens for user connections, and allows for parallel
+     * connection for multiple users to connect to at once.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Connection conn = null;
         try{
